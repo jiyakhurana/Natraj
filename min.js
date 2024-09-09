a0631a028679f9c9ff8b75c6edd0d7b125f1205a
jiyakhurana <162807219+jiyakhurana@users.noreply.github.com>
2024-09-09 15:03:06 +0530

    added all files of Natraj Website.

---
+++ min.js
@@ -0,0 +1,29 @@
+
+//active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll() = function() {
+    if(document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+
+
+//nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a) {
+    a.addEventListener("click", function() {
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
